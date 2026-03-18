85bb8eb1f525d3d5c8259badd9e8f87ac3d30720
lizz <home@lizz.dev>
2026-03-18 23:45:08 +0200

    chore: update dependencies

--- composeApp/src/iosMain/kotlin/com/cmm/certificates/data/xlsx/XlsxParser.ios.kt
+++ composeApp/src/iosMain/kotlin/com/cmm/certificates/data/xlsx/XlsxParser.ios.kt
@@ -1,7 +1,5 @@
 package com.cmm.certificates.data.xlsx
 
-import com.cmm.certificates.data.xlsx.RegistrationEntry
-
 actual object XlsxParser {
     actual fun parse(path: String): List<RegistrationEntry> {
         throw UnsupportedOperationException("XLSX parsing is only supported on JVM desktop targets for now.")
