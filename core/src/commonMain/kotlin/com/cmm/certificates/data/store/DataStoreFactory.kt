b812e657a39aa1470c81170cab7480bd0c70ca75
lizz <home@lizz.dev>
2026-03-28 14:31:22 +0200

    fix: datastore test

--- core/src/commonMain/kotlin/com/cmm/certificates/data/store/DataStoreFactory.kt
+++ core/src/commonMain/kotlin/com/cmm/certificates/data/store/DataStoreFactory.kt
@@ -4,11 +4,17 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import okio.FileSystem
+import okio.SYSTEM
 import okio.Path.Companion.toPath
 
 fun createDataStore(producePath: () -> String): DataStore<Preferences> =
     PreferenceDataStoreFactory.createWithPath(
-        produceFile = { producePath().toPath() },
+        produceFile = {
+            producePath().toPath().also { path ->
+                path.parent?.let(FileSystem.SYSTEM::createDirectories)
+            }
+        },
     )
 
 suspend fun clearDataStore(dataStore: DataStore<Preferences>) {
